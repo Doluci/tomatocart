14e9ea17dbd3b78595698bf3cdec8a33ceddc342
info <info@tomatocart.com>
2010-07-19 14:17:17 +0000

    add ajax shopping cart

--- tomatocart/trunk/templates/glass_gray/modules/content/new_products.php
+++ tomatocart/trunk/templates/glass_gray/modules/content/new_products.php
@@ -16,9 +16,10 @@
 <!-- module new_products start //-->
 <div class="moduleBox">
   <h6><?php echo $osC_Box->getTitle(); ?></h6>
-  
-  <div class="content">  
-    <?php 
+
+  <div class="content">
+    <?php
+
       if ($current_category_id < 1) {
         $Qproducts = $osC_Database->query('select p.products_id, p.products_tax_class_id, p.products_price, pd.products_name, pd.products_keyword, i.image from :table_products p left join :table_products_images i on (p.products_id = i.products_id and i.default_flag = :default_flag), :table_products_description pd where p.products_status = 1 and p.products_id = pd.products_id and pd.language_id = :language_id order by p.products_date_added desc limit :max_display_new_products');
       } else {
@@ -27,40 +28,49 @@
         $Qproducts->bindTable(':table_categories', TABLE_CATEGORIES);
         $Qproducts->bindInt(':parent_id', $current_category_id);
       }
-    
+
       $Qproducts->bindTable(':table_products', TABLE_PRODUCTS);
       $Qproducts->bindTable(':table_products_images', TABLE_PRODUCTS_IMAGES);
       $Qproducts->bindTable(':table_products_description', TABLE_PRODUCTS_DESCRIPTION);
       $Qproducts->bindInt(':default_flag', 1);
       $Qproducts->bindInt(':language_id', $osC_Language->getID());
       $Qproducts->bindInt(':max_display_new_products', MODULE_CONTENT_NEW_PRODUCTS_MAX_DISPLAY);
-    
+
       if (MODULE_CONTENT_NEW_PRODUCTS_CACHE > 0) {
         $Qproducts->setCache('new_products-' . $osC_Language->getCode() . '-' . $osC_Currencies->getCode() . '-' . $current_category_id, MODULE_CONTENT_NEW_PRODUCTS_CACHE);
       }
       $Qproducts->execute();
-    
+
       if ($Qproducts->numberOfRows()) {
         $i = 0;
         while ($Qproducts->next()) {
           if(($i % 3 == 0) && ($i != 0))
             echo '<div style="clear:both"></div>';
-    
+
           $osC_Product = new osC_Product($Qproducts->valueInt('products_id'));
-          
-          echo '<div style="margin-top: 10px; float:left; width: 33%; text-align: center">' .
-                 '<span style="display:block; height: 32px; text-align: center">' . osc_link_object(osc_href_link(FILENAME_PRODUCTS, $Qproducts->value('products_id')), $Qproducts->value('products_name')) . '</span>' . 
-                 osc_link_object(osc_href_link(FILENAME_PRODUCTS, $Qproducts->value('products_id')), $osC_Image->show($Qproducts->value('image'), $Qproducts->value('products_name'))) . 
-                 '<span style="display:block; padding: 3px; text-align: center">' . $osC_Product->getPriceFormated(true) . '</span>' .
-                 osc_link_object(osc_href_link(FILENAME_PRODUCTS, $Qproducts->value('products_id') . '&action=cart_add'), osc_draw_image_button('button_add_to_cart.png', $osC_Language->get('button_add_to_cart'))) .
-               '</div>';
-    
+
+          $content = '<div style="margin-top: 10px; float:left; width: 33%; text-align: center">' .
+                      '<span style="display:block; height: 32px; text-align: center">' . osc_link_object(osc_href_link(FILENAME_PRODUCTS, $Qproducts->value('products_id')), $Qproducts->value('products_name')) . '</span>';
+          if ($osC_Product->isSimple()) {
+            $content .= osc_link_object(osc_href_link(FILENAME_PRODUCTS, $Qproducts->value('products_id')), $osC_Image->show($Qproducts->value('image'), $Qproducts->value('products_name')), 'id="productImage' . $Qproducts->value('products_id') . '"');
+            $content .= '<span style="display:block; padding: 3px; text-align: center">' . $osC_Product->getPriceFormatedShow(true) . '</span>';
+            $content .= osc_link_object(osc_href_link(FILENAME_PRODUCTS, $Qproducts->valueInt('products_id') . '&action=cart_add'), osc_draw_image_button('button_add_to_cart.png', $osC_Language->get('button_add_to_cart'), 'class="ajaxAddToCart" pid="' . $Qproducts->value('products_id') . '"'));
+          }else {
+            $content .= osc_link_object(osc_href_link(FILENAME_PRODUCTS, $Qproducts->value('products_id')), $osC_Image->show($Qproducts->value('image'), $Qproducts->value('products_name')));
+            $content .= '<span style="display:block; padding: 3px; text-align: center">' . $osC_Product->getPriceFormatedShow(true) . '</span>';
+            $content .= osc_link_object(osc_href_link(FILENAME_PRODUCTS, $Qproducts->valueInt('products_id') . '&action=cart_add'), osc_draw_image_button('button_add_to_cart.png', $osC_Language->get('button_add_to_cart')));
+          }
+
+          $content .= '</div>';
+
+          echo $content;
+
           $i++;
         }
-        
+
         echo '<div style="clear:both"></div>';
       }
-    
+
       $Qproducts->freeResult();
     ?>
   </div>
