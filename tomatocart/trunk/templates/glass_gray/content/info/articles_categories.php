15b6592bd7ba42e68b76248d94fc6f477b044006
info@tomatocart.com <info@tomatocart.com@0f9d7f4a-e5ea-8ea1-ca89-10776ce32361>
2010-07-23 05:36:50 +0000

    Fix article layout bug

--- tomatocart/trunk/templates/glass_gray/content/info/articles_categories.php
+++ tomatocart/trunk/templates/glass_gray/content/info/articles_categories.php
@@ -32,7 +32,7 @@
       }
   ?>
   
-    <p style="height:0px"><?php echo substr($Qarticles->value('articles_description'), 0, 300) . ((strlen($Qarticles->valueProtected('articles_description')) >= 100) ? '..' : ''); ?></p>
+    <p><?php echo substr($Qarticles->value('articles_description'), 0, 300) . ((strlen($Qarticles->valueProtected('articles_description')) >= 100) ? '..' : ''); ?></p>
 
       <div style="clear: both;"></div>
     </div>
