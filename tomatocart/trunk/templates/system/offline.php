88f7e59c4df6e46853f0de387a5849856ce2ba52
tomatocart <tomatocart@0f9d7f4a-e5ea-8ea1-ca89-10776ce32361>
2010-11-30 17:00:12 +0000

    fix bug #263 -- Website in Maintenance

--- tomatocart/trunk/templates/system/offline.php
+++ tomatocart/trunk/templates/system/offline.php
@@ -10,6 +10,7 @@
   it under the terms of the GNU General Public License v2 (1991)
   as published by the Free Software Foundation.
 */
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="<?php echo $osC_Language->getTextDirection(); ?>" xml:lang="<?php echo $osC_Language->getCode(); ?>" lang="<?php echo $osC_Language->getCode(); ?>">
@@ -20,15 +21,15 @@
 
     <meta name="Generator" content="TomatoCart" />  
 
-    <link rel="shortcut icon" href="templates/system/images/tomatocart.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="<?php echo osc_href_link('templates/system/images/tomatocart.ico', '', '', false, false, true); ?>" type="image/x-icon" />
 
-    <link rel="stylesheet" href="templates/system/stylesheet.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo osc_href_link('templates/system/stylesheet.css', '', '', false, false, true); ?>" type="text/css" />
   </head>
   <body id="offline">
     <div id="pageContent">
       
       <div class="content">
-        <img src= "images/login_logo.png"/> 
+        <img src= "<?php echo osc_href_link('images/login_logo.png', '', '', false, false, true); ?>"/> 
         <h1><?php echo STORE_NAME; ?></h1>
         
         <p><?php echo $osC_Language->get('introduction_maintenance_mode_text'); ?></p>
@@ -46,7 +47,9 @@
             <li><?php echo osc_draw_label($osC_Language->get('field_password'), 'user_password') . osc_draw_password_field('user_password'); ?></li>
           </ol>
           
-          <p align="center"><?php echo osc_draw_image_submit_button('button_login.gif', $osC_Language->get('button_sign_in')); ?></p>
+          <p align="center">
+            <input type="submit" id="submit" value="<?php echo $osC_Language->get('button_sign_in'); ?>" />
+          </p>
           
         </form>
       </div>
