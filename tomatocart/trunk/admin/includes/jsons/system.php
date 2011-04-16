946c2b91f46bdf24402a5f11d819a1dfe71b61f9
tomatocart <tomatocart@gmail.com>
2011-04-16 16:19:08 +0000

    improve the live feed feature, display the live feed only once

--- tomatocart/trunk/admin/includes/jsons/system.php
+++ tomatocart/trunk/admin/includes/jsons/system.php
@@ -27,21 +27,31 @@
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
       
-      
       $response = curl_exec($ch);
       if (!curl_errno($ch)) {
         $data = trim(substr($response, strpos($response, "\r\n\r\n", strpos(strtolower($response), 'content-length:'))));
         
         $osC_XML = new osC_XML($data);
         $definitions = $osC_XML->toArray();
-        
+
         $items = $definitions['rss']['channel']['item'];
         $feeds = '';
         if (is_array($items) && sizeof($items) > 0) {
           $feeds = '<a href="' . $items['link'] . '" target="_blank"><h1>' . $items['title'] . '</h1></a>' . '<p>' . $items['description'] . '</p><p align="right"><a href="' . $items['link'] . '" target="_blank">Read More...</a></p>';
+
+          require_once('includes/classes/desktop_settings.php');
+          $toC_Desktop_Settings = new toC_Desktop_Settings();
+          
+          $last_feed = (int) $toC_Desktop_Settings->getLastLiveFeed();
+          $current_feed = (int) $items['pubDate'];
+
+          if ($current_feed > $last_feed) {
+            $response = array('success' => true, 'feeds' => $feeds);
+            $toC_Desktop_Settings->setLastLiveFeed($current_feed);
+          } else {
+            $response = array('success' => false);
+          }
         }
-        
-        $response = array('success' => true, 'feeds' => $feeds);
       } else {
         $response = array('success' => false);
       } 
